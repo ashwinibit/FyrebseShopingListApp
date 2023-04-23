3b55effe8c761a793c62bdce1c22aa7151d98514
ashwinibit <ashwinikumarrbhilai@gmail.com>
2023-04-24 05:22:10 +0530

    Page design

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+
+const inputFieldDt = document.getElementById("input-field")
+const addButtonDt = document.getElementById("add-button")
+
+addButtonDt.addEventListener("click", function(){
+    let inputVal = inputFieldDt.value 
+    console.log(inputVal)
+});
+
